2efecf7404a65c4c7671c17fa659e378c75c0177
Florian <florian.tellier02@gmail.com>
2021-03-02 18:55:01 +0100

    ajoue animation header

--- src/components/drag.js
+++ src/components/drag.js
@@ -9,25 +9,27 @@ const Drag = () => (
     <div className="drag_coontainer" >
 
     <motion.div className="drag" drag="x" dragConstraints={{ left: -1100, right: 0 }} >
-    <div>
-       <Image alt="image" filename="image1.jpg" />
-       <Link to="/project_1">
-         Projet
-       </Link>
+    <div className="images_container">
+      <div className="img_invi"> </div>
+      <Image alt="image" filename="image1.jpg" />
+      <Link to="/project_1">Projet</Link>
     </div>
-    <div>
+    <div className="images_container" >
+       <div className="img_invi"> </div>
        <Image alt="image" filename="image2.jpg" />
        <Link to="/project_2">
          Projet
        </Link>
     </div>
-    <div>
+    <div className="images_container" >
+    <div className="img_invi"> </div>
     <Image alt="image" filename="image3.jpg" />
        <Link to="/project_3">
          Projet
        </Link>
     </div>
-    <div>
+    <div className="images_container" >
+    <div className="img_invi"> </div>
     <Image alt="image" filename="image4.jpg" />
        <Link to="/project_4">
          Projet
